324ff1026a50ba6d41976f1fc7af0cb4c2522ee0
Edoardo Pasca <14138589+paskino@users.noreply.github.com>
2025-06-27 08:34:15 +0100

    add initital file browsing stuff

---
+++ README.md
@@ -0,0 +1,5 @@
+TL;DR you should copy-paste stuff from [PETRIC-backend:rank/compute.py:73-97](https://github.com/SyneRBI/PETRIC-backend/blob/f5cf0ddf2ada93c6491f6d834241463e6b0ef2c5/rank/compute.py#L73-L97) into a script and run it on tomography.stfc.ac.uk:/opt/runner/logs/*/*/*/events.out.tfevents.* 
+
+tomography.stfc.ac.uk is 130.246.81.43 
+can ssh in as ofn77899
+
---
+++ data/retrieve_data.py
@@ -0,0 +1,52 @@
+# import logging
+# from collections import defaultdict
+from pathlib import Path, PurePath
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+# from tensorboard.backend.event_processing.event_accumulator import SCALARS, EventAccumulator
+# from tqdm import tqdm
+import os
+
+
+teams = ['MaGeZ',  'SOS',  'Tomo-Unimib',  'UCL-EWS']
+phantoms = ['Mediso_NEMA_lowcounts', 'Vision600_Hoffman']
+
+algoteams = {
+    'MaGeZ': ['ALG1', 'ALG2', 'ALG3'],
+    'SOS': ['SAGA_final2', 'SVRG_final'],
+    'Tomo-Unimib': ['LP_final'],
+    'UCL-EWS': ['EWS-GD', 'EWS-SAGA', 'EWS-SGD']
+}
+
+base_path = Path('/opt/runner/logs')
+
+def get_data(path: PurePath):
+    """
+    Retrieve data from TensorBoard event files.
+    """
+    # event_acc = EventAccumulator(path, size_guidance={SCALARS: 0})
+    # event_acc.Reload()
+
+    # # Get scalar tags
+    # tags = event_acc.Tags()[SCALARS]
+    
+    # # Initialize a dictionary to hold the data
+    # data = {}
+    
+    # for tag in tags:
+    #     events = event_acc.Scalars(tag)
+    #     steps = [event.step for event in events]
+    #     values = [event.value for event in events]
+    #     data[tag] = (steps, values)
+    print (f"Processing path: {path}")
+
+
+for team, algos in algoteams.items():
+    for algo in algos:
+        for phantom in phantoms:
+            path = base_path / team / algo / phantom
+            if path.exists():
+                get_data(path)
+            else:
+                print(f"Path {path} does not exist.")
