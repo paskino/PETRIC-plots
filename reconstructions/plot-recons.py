2208031efd2718d53d1ab6290870cae4ac7284b9
Edoardo Pasca <14138589+paskino@users.noreply.github.com>
2025-08-06 15:44:29 +0200

    fully parametrized

--- reconstructions/plot-recons.py
+++ reconstructions/plot-recons.py
@@ -24,6 +24,7 @@ Tomo-Unimib/LP_final/Mediso_NEMA_lowcounts/iter_0007.v
 UCL-EWS/EWS_GD/Mediso_NEMA_lowcounts/iter_0008.v
 UCL-EWS/EWS_SAGA/Mediso_NEMA_lowcounts/iter_0072.v
 UCL-EWS/EWS_SGD/Mediso_NEMA_lowcounts/iter_0189.v""",
+                "diff_range": (-0.01, 0.01),
                 "VOI" : [
                     {"fname": "VOI_2.v"},
                     {"fname": "VOI_3.v"},
@@ -33,7 +34,7 @@ UCL-EWS/EWS_SGD/Mediso_NEMA_lowcounts/iter_0189.v""",
             "Hoffman": {
                 "shape": (159, 169, 169),
                 "crop": (79, slice(30, 140), slice(30, 140)),
-                "reference": "Hoffman_75.v",
+                "reference": "reference_image.v",
                 "dirname": "/Users/edoardo.pasca/Documents/Papers/PETRIC/PETRIC-paper-data/Hoffman",
                 "algos": """MaGeZ/ALG1/Vision600_Hoffman/iter_0450.v
 MaGeZ/ALG2/Vision600_Hoffman/iter_0450.v
@@ -44,6 +45,7 @@ Tomo-Unimib/LP_final/Vision600_Hoffman/iter_0033.v
 UCL-EWS/EWS_GD/Vision600_Hoffman/iter_0177.v
 UCL-EWS/EWS_SAGA/Vision600_Hoffman/iter_0337.v
 UCL-EWS/EWS_SGD/Vision600_Hoffman/iter_0453.v""",
+                "diff_range": (-0.1, 0.1),
                 "VOI" : [
                     {"fname": "VOI_GM.v"},
                     {"fname": "VOI_ventricles.v"},
@@ -84,7 +86,8 @@ def get_phantom_data(phantom_name, phantoms):
 
 # %%
 # Mediso NEMA low counts
-dphantom = "NEMA"
+# dphantom = "NEMA"
+dphantom = "Hoffman"
 
 algos, reference , VOIs = get_phantom_data(dphantom, phantoms)
 # NEMA_reference = np.fromfile(fname, dtype=np.float32).reshape(shape)
@@ -94,58 +97,31 @@ show2D(reference.__getitem__(phantoms[dphantom]['crop']),
        title = "reference",
        origin="upper-left", 
        cmap="cubehelix_r", fix_range=True)
-# %%
 
-txt = """MaGeZ/ALG1/Mediso_NEMA_lowcounts/iter_0064.v
-MaGeZ/ALG2/Mediso_NEMA_lowcounts/iter_0055.v
-MaGeZ/ALG3/Mediso_NEMA_lowcounts/iter_0054.v
-SOS/SAGA_final2/Mediso_NEMA_lowcounts/iter_0002.v
-SOS/SVRG_final/Mediso_NEMA_lowcounts/iter_0003.v
-Tomo-Unimib/LP_final/Mediso_NEMA_lowcounts/iter_0007.v
-UCL-EWS/EWS_GD/Mediso_NEMA_lowcounts/iter_0008.v
-UCL-EWS/EWS_SAGA/Mediso_NEMA_lowcounts/iter_0072.v
-UCL-EWS/EWS_SGD/Mediso_NEMA_lowcounts/iter_0189.v"""
-NEMA = {}
-for line in txt.split("\n"):
-    team, algo, phantom, string = line.split("/")
-    # Match exactly 4 digits after "iter_"
-    match = re.search(r'iter_(\d{4})', string)
-    if match:
-        number = match.group(1)  # Returns "0055"
-        fname = os.path.abspath(
-            os.path.join(
-            "/Users/edoardo.pasca/Documents/Papers/PETRIC/PETRIC-paper-data/", 
-            team, algo, phantom, 
-                f"iter_{number}.v")
-        )
-        # load as np array from the interfile
-        shape = (75, 155, 155)
-        tmp = np.fromfile(fname, dtype=np.float32).reshape(shape)
-        NEMA[f"{phantom}/{team}/{algo}"] = tmp.reshape(shape)
 
 # %%
 
 nemafig = show2D([v.__getitem__(phantoms[dphantom]['crop']) for k,v in algos.items()], 
     #    slice_list=(0,25), 
        num_cols=3,
-       title = [k for k,v in NEMA.items()],
+       title = [k for k,v in algos.items()],
        origin="upper-left", 
        cmap="cubehelix_r", fix_range=True)
 # %%
 nemafig = show2D([(v - reference).__getitem__(phantoms[dphantom]['crop']) for k,v in algos.items()], 
     #    slice_list=(0,25), 
        num_cols=3,
-       title = [k for k,v in NEMA.items()],
+       title = [k for k,v in algos.items()],
        origin="upper-left", 
-       cmap="seismic", fix_range=(-0.01, 0.01))
+       cmap="seismic", fix_range=phantoms[dphantom]['diff_range'])
 
 # %%
 # calculate the statistics of the VOI
 
 fig, axs = plt.subplots(3, 3)
 for kk,voi in VOIs.items():
-    for ax, k in zip(fig.get_axes(), NEMA.keys()):
-        ax.hist(NEMA[k][voi > 0], bins=20, label=kk, alpha=1, histtype='step')
+    for ax, k in zip(fig.get_axes(), algos.keys()):
+        ax.hist(algos[k][voi > 0], bins=20, label=kk, alpha=1, histtype='step')
 ax.legend(prop={'size': 10})
 fig.suptitle(f"{dphantom}")
 fig.show()
