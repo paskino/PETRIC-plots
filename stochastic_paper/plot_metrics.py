225ac64bbbbb3ccde52d94cb958674cb777fc445
Edoardo Pasca <14138589+paskino@users.noreply.github.com>
2025-09-05 16:33:46 +0100

    added scripts

---
+++ stochastic_paper/plot_metrics.py
@@ -0,0 +1,175 @@
+#%%
+import matplotlib.pyplot as plt
+import pickle
+import os
+from warnings import warn
+
+ROIS = [
+"RMSE_whole_object",
+"RMSE_background",
+"AEM_VOI_sphere1",
+"AEM_VOI_sphere3",
+"AEM_VOI_sphere5",
+]
+algorithms = [ "SG", "SAG", "SAGA", "SVRG", "LSVRG", "SOS-SAGAfinal2"]
+# algorithms = ['SAG', 'SAGA', 'SG', 'SVRG']
+# algorithms = ['SOS-SAGAfinal2']
+subsets = [21,42,63]
+# subsets = [42]
+dataset = ['mMR_NEMA_lowcounts']
+
+#%%
+# Open the file in read-binary mode
+with open('allresults_lowcounts2.pkl', 'rb') as file:
+    # Deserialize the dictionary from the file
+    loaded_dict = pickle.load(file)
+
+#%%
+# plot one algorithm WRT number of subsets
+linestyles = ['solid', 'dotted', 'dashed', 'solid', 'dotted', 'dashed']
+linecolors = ['C0', 'C1', 'C2', 'C3', 'C4', 'C5']
+# import pysnooper
+# @pysnooper.snoop()
+def _plot_it(data, algs: list, num_subsets, which, dataset="mMR_NEMA", linecolors=linecolors, 
+             linestyles=linestyles, 
+             yscale='linear', figsize=None, xlim=None, legend_loc=None, savefig=False,
+            title:list=None, ylabel:str=None, show_legend:bool=True,
+            additional_text:tuple=None):
+    """
+    legend_loc: 'upper left', 'upper right', 'lower left', 'lower right'
+    """
+    plt.figure(figsize=figsize)
+    if not isinstance(num_subsets, list):
+        num_subsets = [num_subsets]
+    for alg_idx, alg in enumerate(algs):
+        for ns_idx,ns in enumerate(num_subsets):
+            idx = f"{alg}_{dataset}_{ns}"
+            # print (f"processing {idx}")
+            algdata = data[idx]
+            tmp = algdata['iteration_num']
+            if which == 'objective':
+                x = [ el/num_subsets[ns_idx] for el in algdata['iteration_num']]
+                y = algdata['objective']
+                xlabel = 'epoch'
+            else:
+                # why this has one more than the data passes?
+                
+                if alg in ["SAG", "SAGA"]:
+                    x = algdata['data_passes']
+                    # the last one is the one at 80 epochs
+                    y = algdata[which][:-1]
+                    # print (f"{alg} {len(x)} {len(y)}")
+                elif alg == "SOS-SAGAfinal2":
+                    # we do 5 full gradients in SOS but the number of data passes is not accounted
+                    # for correctly
+                    x = [el + 5 for el in algdata['data_passes'] ]
+                    x = [1,2,3,4,5] + x
+                    y = algdata[which][:-1]
+                    
+                elif alg in ['SVRG', 'LSVRG']:
+                    x = algdata['data_passes']
+                    # This failed regularly without the [1:]
+                    y = algdata[which][:-1]
+                    # print (f"{alg} {len(x)} {len(y)}")
+                elif alg == 'SG':
+                    x = algdata['data_passes']
+                    # This failed regularly without removing one from the data
+                    y = algdata[which][:-1]
+                    # print (f"{alg} {len(x)} {len(y)}")
+                # x = [el for el in range(len(y))]
+                xlabel = 'data passes'
+            
+            #assert len(x) == len(y), f"{idx} {which}: len(x) != len(y) {len(x)}=!{len(y)} "
+            if len(x) == len(y):
+                ii = ns_idx + len(num_subsets) * alg_idx
+                if title is None:
+                    ttl = f"{alg} {ns} subsets"
+                else:
+                    ttl = title[ii]
+                
+                # print (f"{alg} ii {ii}, alg_idx {alg_idx} ns_idx {ns_idx}")
+                fkwargs = {'linestyle': linestyles[ii] ,
+                           'color': linecolors[ii]}
+                        
+                plt.plot(x,y, label=ttl, **fkwargs)
+            else:
+                warn(f"ERROR: {alg} {idx} {which}: len(x) != len(y) {len(x)}=!{len(y)} ")
+
+    legend_size = 14
+    if show_legend:
+        if legend_loc is not None:
+            plt.legend(loc=legend_loc, prop={'size': legend_size}, ncol=2)
+        else:
+            plt.legend(prop={'size': legend_size},ncol=2)
+    plt.xlabel(xlabel)
+    if xlim is not None:
+        plt.xlim(xlim)
+    if ylabel is None:
+        ylabel = which
+    plt.ylabel(ylabel)
+    plt.yscale(yscale)
+    # plt.title(which)
+    if additional_text is not None:
+        plt.text(*additional_text)
+
+    # grids
+    plt.grid(visible=True, which='both', axis='both', alpha=0.5, linewidth=0.5)
+    if savefig:
+        print("saving fig")
+        gcf = plt.gcf()
+        gcf.savefig(f"{which}_{num_subsets}.png", format='png')
+    plt.show()
+    
+#%%
+_linestyles = ['solid', 'dotted', 'dashed']
+_algos = algorithms
+subsets = [21,42,63]
+
+def get_linestyles(subsets, algos=_algos, linestyles=_linestyles):
+    ls = []
+    for algo in algos:
+        for i,ds in enumerate(subsets):
+            ls.append(linestyles[i])
+    return ls
+
+# linecolors = [f'C{i}' for i in [0,0,0,1,1,1,2,2,2,3,3,3,4,4,4,5,5,5]]
+def get_linecolors(subsets, algos):
+    lc = []
+    for i,_ in enumerate(algos):
+        for _ in subsets:
+            lc.append(f'C{i}')
+    return lc
+
+# linestyles = get_linestyles(subsets)
+# print (linestyles)
+# print (get_linecolors(subsets, _algos))
+#%%
+import matplotlib
+matplotlib.rcParams.update({'font.size': 20})
+matplotlib.rcParams.update({'lines.linewidth': 3.0})
+algos = [ "SAGA", "SOS-SAGAfinal2"]
+_algos = algos
+savefig = True
+#"SG", "SAG", "SAGA", "SVRG", "LSVRG", 
+_plot_it(loaded_dict, _algos , subsets, 
+         which=ROIS[0], dataset="mMR_NEMA_lowcounts",
+         linestyles=get_linestyles(subsets, algos),
+         linecolors=get_linecolors(subsets, algos), 
+         figsize=(9,7), yscale="log", xlim=(0,30),
+         legend_loc='upper right', savefig=savefig,
+        title=["SAGA-1 (N=21)", "SAGA-1 (N=42)", "SAGA-1 (N=63)",
+              "SAGA-SOS (N=21)", "SAGA-SOS (N=42)", "SAGA-SOS (N=63)"],
+        ylabel="RMSE whole object",
+        additional_text=(2,2e-3,"A"))
+
+#%%
+_plot_it(loaded_dict, _algos, subsets, 
+         which=ROIS[2], dataset="mMR_NEMA_lowcounts",
+         linestyles=get_linestyles(subsets, algos),
+         linecolors=get_linecolors(subsets, algos),  figsize=(9,7), yscale="log", xlim=(0,30),
+         legend_loc='lower left', savefig=savefig, 
+        title=["SAGA-1 21 subsets", "SAGA-1 42 subsets", "SAGA-1 63 subsets",
+              "SAGA-SOS 21 subsets", "SAGA-SOS 42 subsets", "SAGA-SOS 63 subsets"],
+        ylabel="AEM sphere 1", show_legend=False,
+        additional_text=(2,3e-3,"B"))
+# %%
---
+++ stochastic_paper/plot_pet.py
@@ -0,0 +1,103 @@
+# %%
+import os
+import numpy as np
+import re
+import glob
+
+print (__file__, os.path.dirname(__file__))
+#%%
+os.chdir(os.path.dirname(__file__))
+from cil.utilities.display import show2D
+import matplotlib.pyplot as plt
+
+
+crop = (71, slice(35, 115), slice(37, 117))
+# crop = (71, None, None)
+
+data = {}
+for alg in ["SAGA", "SOS-SAGAfinal2"]:
+    for ns in [21,42,63]:
+        key = f"{alg}_{ns}"
+        data[key] = np.load(f"{key}.npy")
+
+reference = np.load("reference.npy")
+whole_object = np.load("whole_object.npy")
+spheres = {
+    "sphere1": np.load("VOI_sphere1.npy"),
+    "sphere5": np.load("VOI_sphere5.npy"),
+    "sphere3": np.load("VOI_sphere3.npy"),
+}
+
+#%%
+fig, axs = plt.subplots(2, 3, figsize=(12, 6.5))
+# Remove all spacing between subplots
+crop = (71, slice(30, 150), slice(37, 190))
+
+for kv, ax in zip(data.items(), axs.flatten()):
+    k, v = kv
+    mask = whole_object.__getitem__(crop) > 0
+    img[~mask] = 0
+    v = v.__getitem__(crop)
+    v[~mask] = 0
+    ref = reference.__getitem__(crop)
+    ref[~mask] = 0
+    img = ((v - ref)/ref)
+    np.nan_to_num(img, nan=0, posinf=0, neginf=0)
+
+    # img = v.__getitem__(crop)
+    img = np.squeeze(img)
+    # img = v-reference
+    algo, ns = k.split("_")
+    if algo == "SOS-SAGAfinal2":
+        algo = "SAGA-SOS"
+    elif algo == "SAGA":
+        algo = "SAGA-1"
+    title = f"{algo} (N={ns})"
+    cmap = "seismic"
+    # cmap = "gray"
+    # vmin, vmax = (-2e-4, 2e-4)
+    vmin, vmax = (-5e-2, 5e-2)
+    sp = ax.imshow(img, cmap=cmap, 
+                   vmin=vmin, vmax=vmax, 
+                   label=title)
+    ax.annotate(title, xy=(3, 12), textcoords='data', ha='left', 
+                fontsize=15)
+    ax.set_xticklabels([])
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_yticklabels([])
+
+cax = plt.axes([0.8, 0.025, 0.02, 0.72])  # Adjust the position of the colorbar
+from matplotlib.ticker import PercentFormatter
+mf = PercentFormatter(xmax=1, decimals=0)
+fig.colorbar(sp, orientation='vertical', 
+             use_gridspec=True,
+             cax=cax,
+             format=mf)
+# Remove tight_layout as it overrides GridSpec spacing
+# fig.tight_layout()
+
+# main_title = f"{dphantom} phantom on {scanner}"
+# fig.suptitle(main_title, fontsize=16)
+# Remove tight_layout and use subplots_adjust to control spacing precisely
+plt.subplots_adjust(wspace=0.03, hspace=0.02, 
+                    left=0.02, right=0.78, 
+                    top=0.75, bottom=0.02)
+# Ensure no spacing and tight layout
+dfig = plt.gcf()
+plt.show()
+dfig.savefig(f"PET_whole_volume_diff.png")
+# %%
+alg = "SAGA_63"
+md = np.load(alg + ".npy")
+
+show2D(md, slice_list=(0,71), fix_range=(0,0.05),
+       title=alg)
+# %%
+alg = "SOS-SAGAfinal2_63"
+show2D(data[alg], slice_list=(0,71), fix_range=(0,0.05),
+       title=alg)
+
+# %%
+show2D(reference, slice_list=(0, 71), fix_range=(0,0.05), title="Reference")
+# %%
